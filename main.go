d86f89f9cc0e00e248bad21cbca74e37317e85e4
Cacahuète <24205659+CacahueteSansSel@users.noreply.github.com>
2023-01-26 15:34:11 +0100

    tasks cors: reintroduced CORS

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ package main
 */
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"log"
 	"neeft_back/database"
@@ -13,9 +14,21 @@ import (
 
 func main() {
 	database.ConnectDb() // Initialize the database if it does not exist (it is created automatically the tables thanks to the migration)
+
 	app := fiber.New()
+
 	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		Next:             nil,
+		AllowOrigins:     "*",
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders:     "",
+		AllowCredentials: false,
+		ExposeHeaders:    "",
+		MaxAge:           0,
+	}))
 
 	routes.SetupRouters(app)
+
 	log.Fatal(app.Listen(":8080"))
 }
